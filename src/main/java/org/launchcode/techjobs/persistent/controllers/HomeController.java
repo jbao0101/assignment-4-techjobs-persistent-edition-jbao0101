bc8444cf8f552308010c691b656816f81be4d6e7
Joe Bao <toj7278@yahoo.com>
2022-06-25 00:48:53 -0500

    stuck on part 3 HomeController

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -1,6 +1,9 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -15,11 +18,12 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @Autowired
+    private EmployerRepository employerRepository;
+
     @RequestMapping("")
     public String index(Model model) {
-
         model.addAttribute("title", "My Jobs");
-
         return "index";
     }
 
@@ -27,6 +31,7 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute(new Job());
+        model.addAttribute("employer", employerRepository.findAll());
         return "add";
     }
 
@@ -38,7 +43,8 @@ public class HomeController {
             model.addAttribute("title", "Add Job");
             return "add";
         }
-
+        newJob.setEmployer(employerRepository.findById(employerId));
+        employerRepository.save();
         return "redirect:";
     }
 
